78ff2f8ba6d56c53467a4cd919ba48a3d2935225
jamilxt <jamilxt@gmail.com>
2020-09-09 17:36:56 +0600

    add Tamil as additional language

--- src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
+++ src/main/webapp/app/shared/language/find-language-from-key.pipe.ts
@@ -5,6 +5,7 @@ export class FindLanguageFromKeyPipe implements PipeTransform {
   private languages: { [key: string]: { name: string; rtl?: boolean } } = {
     bn: { name: 'বাংলা' },
     en: { name: 'English' },
+    ta: { name: 'தமிழ்' },
     // jhipster-needle-i18n-language-key-pipe - JHipster will add/remove languages in this object
   };
 
